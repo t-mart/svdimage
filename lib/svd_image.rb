bbeab37e3651620d2b786d47f2243e21ea551980
Tim Martin <fill in your email>
2012-02-12 07:21:23 -0500

    switch to gsl (best idea evar)

--- lib/svd_image.rb
+++ lib/svd_image.rb
@@ -1,91 +1,44 @@
-require 'matrix'
-
-class Matrix::EigenvalueDecomposition
-
-  def eigenvalues_decr
-    eigenvalues.sort.reverse
-  end
-
-  def eigenvalue_matrix_decr
-    Matrix.diagonal(*(eigenvalues.sort.reverse))
-  end
-
-  def eigenvector_matrix_decr
-    cols = {}
-
-    evalues = eigenvalues #don't compute this each time in following loop
-    evectors = eigenvector_matrix #or this
-    
-    evalues.each_with_index do |evalue, i|
-      cols[evalue] = evectors.column(i)
-    end
-
-    decr_cols = cols.keys.sort.reverse.inject(Array.new) do |working, next_col|
-      working << cols[next_col]
-    end
-
-    Matrix.columns(decr_cols)
-  end
-  
-  def eigenvector_matrix_inv_decr
-    cols = {}
-
-    evalues = eigenvalues #don't compute this each time in following loop
-    evectors = eigenvector_matrix_inv #or this
-    
-    evalues.each_with_index do |evalue, i|
-      cols[evalue] = evectors.column(i)
-    end
-
-    decr_cols = cols.keys.sort.reverse.inject(Array.new) do |working, next_col|
-      working << cols[next_col]
-    end
-
-    Matrix.columns(decr_cols)
-  end
-end
-
-class Matrix
-  #divide the column vectors of this matrix by the first element of that column.
-  #this is helpful when we need to compare 2 augmented non-unique matricies,
-  #such as eigenvector augmentations. only the ratio matters between the
-  #elements of the columns.
-  def normalize_to_top
-    cols = column_vectors
-
-    cols.size.times do |i|
-      cols[i] /= cols[i][0].to_f
-    end
-
-    Matrix.columns( cols )
-  end
-
-end
+require 'gsl'
+include GSL
 
 class SvdImage
+
   VERSION = '1.0.0'
+
   def SvdImage.svd a
-    #steps
-    #  ata = a_transpose * a
-    #  aat = a * a_transpose
-    #  S = ata's eigenvals placed along diagonal in decreasing order
-    #  S = S**0.5 (since its a_transpose * a)
-    #  V = [augmented eigenvectors of ata] (ensure correct order!)
-    #  U = [augmented eigenvectors of aat] (ensure correct order!)
-    #  return U, S, V.t (which = A)
+    begin
+      u, v, s = a.SV_decomp
+      return u, Matrix.diagonal(s), v.transpose
+    rescue ERROR::EUNIMPL
+      #for some reason, gsl hasn't implemented this, yet it seems pretty easy to
+      #fix. here's what's going on:
+      #u is v
+      #v is u
+      #transpose s
+      u, v, s = a.transpose.SV_decomp
+      return v, Matrix.diagonal(s).transpose, u.transpose
+    end
+  end
 
-    ata = a.t * a
-
-    aat = a * a.t
-
-    s = ata.eigen.eigenvalue_matrix_decr ** 0.5
-
-    v_t = ata.eigen.eigenvector_matrix_decr.transpose
-
-    u = aat.eigen.eigenvector_matrix_decr
-
-    return u, s, v_t
+  def SvdImage.svd_truncate a, k
+    #k columns of U
+    #k-wide diagonal s (largest)
+    #k rows of V^T
+    u, s, v_t = SvdImage.svd a
+    return u.submatrix(nil, 0...k),
+      s.submatrix(0...k, 0...k),
+      v_t.submatrix(0...k, nil)
   end
 end
+#http://www.gnu.org/software/gsl/manual/html_node/Singular-Value-Decomposition.html
+#The singular value decomposition of a matrix has many practical uses. The
+#condition number of the matrix is given by the ratio of the largest
+#singular value to the smallest singular value. The presence of a zero
+#singular value indicates that the matrix is singular. The number of
+#non-zero singular values indicates the rank of the matrix. In practice
+#singular value decomposition of a rank-deficient matrix will not
+#produce exact zeroes for singular values, due to finite numerical
+#precision. Small singular values should be edited by choosing a suitable
+#tolerance. 
 
 
--- test/test_svd_image.rb
+++ test/test_svd_image.rb
@@ -4,23 +4,8 @@ require "svd_image"
 class TestSvdImage < MiniTest::Unit::TestCase
 
   def setup
-    #this kinda sucks with how much data i'm entering, but the point of these
-    #tests is to verify that my ruby is following the correct math, and i can
-    #only do that with precomputed values.
-    @a = Matrix[[2,-2],[1,1]]
-    @a_normalized_to_top = Matrix[[1,1],[0.5,-0.5]]
-
-    @aat = @a * @a.t
-    @aat_eigenvalues_decr = [8.0, 2.0]
-    @aat_eigenvalue_matrix_decr = Matrix.diagonal(*@aat_eigenvalues_decr)
-    @aat_eigenvector_matrix_decr = Matrix.columns( [[-1, 0], [0, 1]] )
-
-    @ata = @a.t * @a
-    @ata_eigenvalues_decr = [8.0, 2.0]
-    @ata_eigenvalue_matrix_decr = Matrix.diagonal(*@ata_eigenvalues_decr)
-    @ata_eigenvector_matrix_decr = Matrix.columns( [[-(2**0.5)/2, (2**0.5)/2], [(2**0.5)/2, (2**0.5)/2]] )
-
-    @lots_moar = [Matrix[[1, -2], [-2, 1]],
+    @test_matricies = [Matrix[[1, -2], [-2, 1]],
+      Matrix[[2,-2],[1,1]],
       Matrix[[1, -2/3.0], [1/2.0, -1/6.0]],
       Matrix[[3, 1], [-1, 1]],
       Matrix[[1, 2], [-1, 1]],
@@ -30,61 +15,32 @@ class TestSvdImage < MiniTest::Unit::TestCase
       Matrix[[-4, -4, 2], [3, 4, -1], [-3, -2, 3]],
       Matrix[[3, 4, 0, 0], [4, 3, 0, 0], [0, 0, 1, 3], [0, 0, 4, 5]],
       Matrix[[4, 0, 0, 0], [1, 3, 0, 0], [-1, 1, 2, 0], [1, -1, 1, 1]],
-      Matrix[[2, 0, 0], [0, 3, 0]],
-      Matrix[[2, 1, 0, -1], [0, -1, 1, 1]]]
+      Matrix[[1, 2, 8], [2, 5, 6]],
+      Matrix[[1, 2], [2, 5], [8, 6]],
+      Matrix.I(3),
+      Matrix.I(10),
+      Matrix.ones(3),
+      Matrix.ones(10),
+      Matrix.zeros(3),
+      Matrix.zeros(10)]
   end
 
   def test_svd
-    #u, s, v_t = SvdImage.svd (@a)
-
-    #product = u * s * v_t
-
-    #product = product.map { |element| element.round }
-
-    #assert_equal @a, product, "SVD product does not equal original matrix"
-    @lots_moar.each do |a|
+    @test_matricies.each do |a|
       u, s, v_t = SvdImage.svd a
-
-      puts "", u, s, v_t
       
       product = u * s * v_t
-
-      product = product.map { |element| element.round }
 
       assert_equal a, product, "SVD product does not equal original matrix"
     end
   end
 
-  def test_normalize_to_top
-    assert_equal @a_normalized_to_top, @a.normalize_to_top,
-      "Matrix columns not normalized to first element of each column"
-  end
-
-  def test_eigenvalues_decr
-    evalues = @ata.eigen.eigenvalues_decr
-    assert_equal @ata_eigenvalues_decr , evalues, "Eigenvalues not decreasing"
-
-    evalues = @aat.eigen.eigenvalues_decr
-    assert_equal @aat_eigenvalues_decr , evalues, "Eigenvalues not decreasing"
-  end
-
-  def test_eigenvalue_matrix_decr
-    evalue_matrix = @ata.eigen.eigenvalue_matrix_decr
-    assert_equal @ata_eigenvalue_matrix_decr, evalue_matrix, "Eigenvalue matrix not decreasing"
-
-    evalue_matrix = @aat.eigen.eigenvalue_matrix_decr
-    assert_equal @aat_eigenvalue_matrix_decr, evalue_matrix, "Eigenvalue matrix not decreasing"
-  end
-
-  def test_eigenvector_matrix_decr
-    skip("Skipping until I write a way to compare non-unique eigenvectors")
-
-    evector_matrix = @ata.eigen.eigenvector_matrix_decr
-    assert_equal @ata_eigenvector_matrix_decr.normalize_to_top, evector_matrix.normalize_to_top,
-      "Eigenvector matrix does not match respective decreasing eigenvalues"
-
-    evector_matrix = @aat.eigen.eigenvector_matrix_decr
-    assert_equal @aat_eigenvector_matrix_decr.normalize_to_top, evector_matrix.normalize_to_top,
-      "Eigenvector matrix does not match respective decreasing eigenvalues"
+  def test_svd_truncate
+    #okay, uhm, just make sure there are no errors?
+    @test_matricies.each do |a|
+      u, s, v_t = SvdImage.svd_truncate a, 1
+      
+      product = u * s * v_t
+    end
   end
 end
