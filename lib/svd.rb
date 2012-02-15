e0d9bff6bd3a9d6778a4ad760ba391db1fc11336
Tim Martin <teimu.tm@gmail.com>
2012-02-15 04:40:20 -0500

    add some instructions about using jpgs over pngs
    jpgs are where we're seeing the better compression. i think pngs aren't using
    their palettes correctly or something

--- lib/svd.rb
+++ lib/svd.rb
@@ -7,9 +7,14 @@ require 'svd/svd'
 require 'svd/svd_image'
 
 if __FILE__ == $0
-  svdimg = SvdImage.read "cmyk.low.png", :gray
+  #use jpgs to see decrease in file size!
+  #
+  #i think png isn't utilizing palette because size says the same after
+  #different truncations
 
-  svdimg = svdimg.truncate 10
+  svdimg = SvdImage.read "test1.png", :gray
+
+  svdimg = svdimg.truncate 100
 
   svdimg.write("out.jpg")
 end
